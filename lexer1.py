4305b4d9b525adb56c9be5e38883b51c0a00d2e0
ygor <ygorfcmorais@gmail.com>
2025-01-09 00:21:48 -0300

    organizando

--- lexer1.py
+++ lexer1.py
@@ -3,12 +3,12 @@ import ply.lex as lex
 class Lexer:
     # Lista de tokens
     tokens = [
-    'PROGRAM',
-    'CONST', 'INT', 'FLOAT', 'BOOL', 'STRING',
-    'IF', 'ELSE', 'WHILE', 'PRINT', 'INPUT',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQ', 'NEQ', 'LT', 'GT', 'LEQ', 'GEQ', 'NOT', 'ASSIGN',
-    'LBRACE', 'RBRACE', 'LPAREN', 'RPAREN', 'SEMICOLON',
-    'ID','INTEGER_CONST','FLOAT_CONST','STRING_CONST'
+        'PROGRAM', 'CONST', 'INT', 'FLOAT', 'BOOL', 'STRING',
+        'IF', 'ELSE', 'WHILE', 'PRINT', 'INPUT',
+        'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'EQ', 'NEQ', 
+        'LT', 'GT', 'LEQ', 'GEQ', 'NOT', 'ASSIGN',
+        'LBRACE', 'RBRACE', 'LPAREN', 'RPAREN', 
+        'SEMICOLON', 'ID','INTEGER_CONST','FLOAT_CONST','STRING_CONST'
     ]
 
     # Palavras reservadas
@@ -27,7 +27,6 @@ class Lexer:
     }
 
     # Regras de expressões regulares para tokens simples
-    # Operadores
     t_NOT = r'!'
     t_MINUS = r'-'
     t_PLUS = r'\+'
@@ -39,38 +38,20 @@ class Lexer:
     t_LEQ = r'<='
     t_GT = r'>'
     t_LT = r'<'
-
-    # Delimitadores
     t_LBRACE = r'\{'
     t_RBRACE = r'\}'
     t_LPAREN = r'\('
     t_RPAREN = r'\)'
     t_SEMICOLON = r';'
-    t_ignore = ' \t'
     t_ASSIGN = r'='
-
+    t_ignore = ' \t'
 
     def __init__(self):
-        self.lexer = None
+        self.lexer = lex.lex(module=self)
 
-    # Criar o lexer
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
 
-    # Receber o arquivo code.txt
-    def input(self, data):
-        self.lexer.input(data)
-    
-    # Pegar próximo token
-    def token(self):
-        return self.lexer.token()
-
-    def t_error(self, t):
-        print(f"Erro léxico: Caractere inválido '{t.value[0]}' na linha {t.lineno}")
-        t.lexer.skip(1)
-    
-
-    # Regras de expressões regulares para tokens complexos
     def t_ID(self, t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
         t.type = self.reserved.get(t.value, 'ID')
@@ -78,20 +59,20 @@ class Lexer:
 
     def t_INTEGER_CONST(self, t):
         r'\d+'
-        t.value = int(t.value)
         return t
 
     def t_FLOAT_CONST(self, t):
         r'\d+\.\d+'
-        t.value = float(t.value)
         return t
 
-    # Identifica "" como aprte da string
     def t_STRING_CONST(self, t):
         r'\".*?\"'
-        t.value = t.value[1:-1]
         return t
 
     def t_newline(self, t):
         r'\n+'
-        t.lexer.lineno += len(t.value)
+        pass
+
+    def t_error(self, t):
+        print(f"Erro léxico: Caractere inválido '{t.value[0]}'")
+        t.lexer.skip(1)
