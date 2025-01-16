7cc3844bdb009a1fbe528f12087e12e45b9cf744
Maria Eduarda <dudaa.mendes1502@gmail.com>
2025-01-16 00:37:03 -0300

    adicionando checagem de tipos

--- parser.py
+++ parser.py
@@ -29,17 +29,27 @@ class Parser:
         else:
             p[0] = []
 
+
     def p_decl(self, p):
         '''decl : type ID ASSIGN literal SEMICOLON
                 | type id_list SEMICOLON'''
+        
         if len(p) == 6:  # Declaração de variável com atribuição
+            var_type = p[1]
+            value_type = self.symbol_table.check_type(p[4])  # Função que verifica o tipo de literal
+
+            if value_type != var_type:
+                raise Exception(f"Erro semântico: Tipo incompatível na declaração de '{p[2]}'. Esperado {var_type}, mas encontrado {value_type}.")
+            
             self.symbol_table.declare(p[2], p[1]) 
             self.symbol_table.assign(p[2], p[4])  # Atribui o valor à variável
             p[0] = ('var_decl_with_assignment', p[1], p[2], p[4])
+
         else:  # Declaração de variável
             for var in p[2]:
                 self.symbol_table.declare(var, p[1])
             p[0] = ('var_decl', p[1], p[2])
+
 
     def p_type(self, p):
         '''type : INT
@@ -49,12 +59,14 @@ class Parser:
                 | CONST'''
         p[0] = p[1]
 
+
     def p_id_list(self, p):
         '''id_list : ID id_tail'''
         if len(p) == 3:
             p[0] = [p[1]] + p[2]
         else:
             p[0] = [p[1]]
+
 
     def p_id_tail(self, p):
         '''id_tail : COLON ID id_tail
@@ -64,6 +76,7 @@ class Parser:
         else:
             p[0] = []
 
+
     def p_stmt_list(self, p):
         '''stmt_list : stmt stmt_list
                      | empty'''
@@ -71,6 +84,7 @@ class Parser:
             p[0] = [p[1]] + p[2]
         else:
             p[0] = []
+
 
     def p_stmt(self, p):
         '''stmt : assign_stmt
@@ -80,14 +94,24 @@ class Parser:
                 | while_stmt'''
         p[0] = p[1]
 
+
     def p_assign_stmt(self, p):
         '''assign_stmt : ID ASSIGN exp SEMICOLON'''
+        var_type = self.symbol_table.lookup(p[1])
+    
+        value_type = self.symbol_table.check_type(p[3])  # Função que verifica o tipo da expressão
+
+        if value_type != var_type:
+            raise Exception(f"Erro semântico: Tipo incompatível na atribuição de '{p[1]}'. Esperado {var_type}, mas encontrado {value_type}.")
+    
         self.symbol_table.assign(p[1], p[3])
         p[0] = ('assign', p[1], p[3])
+
 
     def p_print_stmt(self, p):
         '''print_stmt : PRINT LPAREN exp_list RPAREN SEMICOLON'''
         p[0] = ('print', p[3])
+
 
     def p_input_stmt(self, p):
         '''input_stmt : INPUT LPAREN id_list RPAREN SEMICOLON'''
@@ -95,9 +119,16 @@ class Parser:
             self.symbol_table.lookup(var)
         p[0] = ('input', p[3])
 
+
     def p_if_stmt(self, p):
         '''if_stmt : IF LPAREN exp RPAREN LBRACE stmt_list RBRACE else_part'''
+        condition_type = self.symbol_table.check_type(p[3])
+
+        if condition_type != 'bool':
+            raise Exception(f"Erro semântico: Condição 'if' deve ser do tipo 'bool', mas foi encontrado {condition_type}.")
+        
         p[0] = ('if', p[3], p[6], p[8])
+
 
     def p_else_part(self, p):
         '''else_part : ELSE LBRACE stmt_list RBRACE
@@ -107,13 +138,21 @@ class Parser:
         else:
             p[0] = None
 
+
     def p_while_stmt(self, p):
         '''while_stmt : WHILE LPAREN exp RPAREN LBRACE stmt_list RBRACE'''
+        condition_type = self.symbol_table.check_type(p[3])
+
+        if condition_type != 'bool':
+            raise Exception(f"Erro semântico: Condição 'while' deve ser do tipo 'bool', mas foi encontrado {condition_type}.")
         p[0] = ('while', p[3], p[6])
+
 
     def p_exp(self, p):
         '''exp : exp_relational'''
+
         p[0] = p[1]
+
 
     def p_exp_relational(self, p):
         '''exp_relational : exp_arithmetic
@@ -123,19 +162,28 @@ class Parser:
                      | exp_arithmetic LEQ exp_arithmetic
                      | exp_arithmetic EQ exp_arithmetic
                      | exp_arithmetic NEQ exp_arithmetic'''
+       
         if len(p) == 2:
             p[0] = p[1]
         else:
             p[0] = ('relop', p[2], p[1], p[3])
+
 
     def p_exp_arithmetic(self, p):
         '''exp_arithmetic : exp_arithmetic PLUS term
                      | exp_arithmetic MINUS term
                      | term'''
         if len(p) == 4:
+            left_type = self.symbol_table.check_type(p[1])
+            right_type = self.symbol_table.check_type(p[3])
+
+            if left_type != right_type or left_type == 'bool' or right_type == 'bool':
+                raise Exception(f"Erro semântico: Operação aritmética inválida entre tipos {left_type} e {right_type}.")
+
             p[0] = ('binop', p[2], p[1], p[3])
         else:
             p[0] = p[1]
+
 
     def p_term(self, p):
         '''term : term TIMES factor
@@ -146,17 +194,19 @@ class Parser:
         else:
             p[0] = p[1]
 
+
     def p_bool_const(self, p):
         '''BOOL_CONST : TRUE
                      | FALSE'''
         p[0] = p[1]
+
 
     def p_factor(self, p):
         '''factor : ID 
                   | INTEGER_CONST 
                   | FLOAT_CONST 
                   | STRING_CONST 
-                  | BOOL_CONST 
+                  | BOOL_CONST
                   | LPAREN exp RPAREN'''
         if len(p) == 2:
             if p.slice[1].type == 'ID':  # Only lookup if token is ID
@@ -165,9 +215,11 @@ class Parser:
         else:
             p[0] = p[2]
 
+
     def p_empty(self, p):
         '''empty :'''
         p[0] = None
+
 
     def p_literal(self, p):
         '''literal : INTEGER_CONST
@@ -176,12 +228,14 @@ class Parser:
                    | BOOL_CONST'''
         p[0] = p[1]
 
+
     def p_exp_list(self, p):
         '''exp_list : exp exp_tail'''
         if len(p) == 3:
             p[0] = [p[1]] + p[2]
         else:
             p[0] = [p[1]]
+
 
     def p_exp_tail(self, p):
         '''exp_tail : COLON exp exp_tail
@@ -190,6 +244,7 @@ class Parser:
             p[0] = [p[2]] + p[3]
         else:
             p[0] = []
+
 
     def p_error(self, p):
         print(f"Erro de sintaxe: {p.value} na linha {p.lineno}")
--- symbolTable.py
+++ symbolTable.py
@@ -15,7 +15,37 @@ class SymbolTable:
     def lookup(self, name):
         if name not in self.table:
             raise Exception(f"Erro semântico: Variável '{name}' não declarada.")
-        return self.table[name]
+        return self.table[name]['type']
+
+
+    def check_type(self, p):
+        try:
+            return self.lookup(p)
+        except:
+            if isinstance(p, int):  # INTEGER_CONST
+                return 'int'
+            elif isinstance(p, float):  # FLOAT_CONST
+                return 'float'
+            elif isinstance(p, str):  # STRING_CONST
+                if p == 'true' or p == 'false':
+                    return 'bool'
+                return 'str'
+            elif isinstance(p, bool):  # BOOL_CONST
+                return 'bool'
+            elif isinstance(p, tuple):
+                if p[0] == 'binop':
+                    left_type = self.check_type(p[2])
+                    right_type = self.check_type(p[3])
+                    if left_type != right_type:
+                        raise Exception(f"Erro semântico: Operação inválida entre tipos {left_type} e {right_type}.")
+                    return left_type
+                elif p[0] == 'relop':  # Operação relacional
+                    left_type = self.check_type(p[2])
+                    right_type = self.check_type(p[3])
+                    if left_type != right_type:
+                        raise Exception(f"Erro semântico: Comparação inválida entre tipos {left_type} e {right_type}.")
+                    return 'bool'  # Operações relacionais retornam um booleano
+
 
     def __str__(self):
         return str(self.table)
